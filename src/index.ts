b338d85c123a00ea943a5278bcdf10bf1ae0ea13
ItsDizzy <itsdizzy40@gmail.com>
2018-02-18 15:28:44 +0100

    Forgot to export NoTokenError

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { ClassProvider, ExistingProvider, FactoryProvider, InjectionToken, Injector, NormalizedProvider, Provider, ReflectiveInjector, ReflectiveDependency, TypeProvider, ValueProvider } from './injector';
-export { InvalidProviderError, NoAnnotationError, NoMixMultiProviderError, NoProviderError } from './errors';
+export { InvalidProviderError, NoAnnotationError, NoMixMultiProviderError, NoProviderError, NoTokenError } from './errors';
 export { getParentCtor, makeDecorator, makeParamDecorator, makePropDecorator, stringify } from './util';
 export { Inject, Injectable, Optional, Self, SkipSelf } from './decorators';
 export { Reflector } from './reflector';
